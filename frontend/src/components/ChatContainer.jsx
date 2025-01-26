d72319b9bdcba7037c1015de5fde8229e88f3311
Sheel Ganvir <sheelganvir2805@gmail.com>
2025-01-26 12:13:58 +0530

    Update ChatContainer.jsx

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -48,13 +48,13 @@ const ChatContainer = () => {
       <ChatHeader />
 
       <div className="flex-1 overflow-y-auto p-4 space-y-4">
-        {messages.map((message) => (
-          <div
-            key={message._id}
+        {messages && Array.isArray(messages) && messages.map((message, index) => (  // Added index parameter
+          <div 
+            key={message._id} 
             className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}
-            ref={messageEndRef}
+            ref={messages.length - 1 === index ? messageEndRef : null}
           >
-            <div className=" chat-image avatar">
+            <div className="chat-image avatar">
               <div className="size-10 rounded-full border">
                 <img
                   src={
