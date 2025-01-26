b7d5b9c3069b05d372fd5760e8187569a7957051
Sheel Ganvir <sheelganvir2805@gmail.com>
2025-01-26 12:33:38 +0530

    version update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  server: {
+    port: 3000
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+  }
 })
